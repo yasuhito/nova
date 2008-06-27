fb8a3a3bbc256853f01873d1f9777812fc1858ba
Yasuhito TAKAMIYA <yasuhito@takamiya.(none)>
2008-06-27 17:47:48 +0900

    heartbeat_server を追加。まだ期待通りには動いてないみたいだけど。。。

---
+++ heartbeat_server.erl
@@ -0,0 +1,30 @@
+-module(heartbeat_server).
+-export([start/0, rpc/2]).
+
+
+start() ->
+    process_flag(trap_exit, true),
+    spawn( fun() -> loop( [] ) end ).
+
+
+rpc( Pid, Request ) ->
+    Pid ! { self(), Request },
+    receive
+        { Pid, Response } ->
+            Response
+    end.
+
+
+loop( X ) ->
+    receive
+        { From, spawn } ->
+            io:format( "SPAWN~n" ),
+            From ! { self(), lib_nova:spawn_all( "nova@hongo", 0, 10 ) },
+            loop( X );
+        { 'EXIT', SomePid, Reason } ->
+            io:format( "EXIT ~p ~p~n", [SomePid, Reason] ),
+            loop( X );
+        Any ->
+            io:format( "Received:~p~n", [Any] ),
+            loop( X )
+    end.
