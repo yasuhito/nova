f67c09c82de232693c970102b1f136b3c855d76f
Yasuhito TAKAMIYA <yasuhito@yasuhito-takamiya-no-macbook.local>
2008-06-27 23:21:04 +0900

    fixed.

--- heartbeat_server.erl
+++ heartbeat_server.erl
@@ -1,29 +1,23 @@
--module(heartbeat_server).
--export([start/0, rpc/2]).
+-module( heartbeat_server ).
+-export( [ start/0 ] ).
 
 
 start() ->
-    process_flag(trap_exit, true),
-    spawn( fun() -> loop( [] ) end ).
+    spawn( fun() -> main() end ).
 
 
-rpc( Pid, Request ) ->
-    Pid ! { self(), Request },
-    receive
-        { Pid, Response } ->
-            Response
-    end.
+main() ->
+    process_flag( trap_exit, true ),
+    lib_nova:spawn_all( "nova@hongo", 0, 10 ),
+    loop( [] ).
 
 
 loop( X ) ->
     receive
-        { From, spawn } ->
-            From ! { self(), lib_nova:spawn_all( "nova@hongo", 0, 10 ) },
-            loop( X );
         { 'EXIT', SomePid, Reason } ->
-            io:format( "EXIT ~p ~p~n", [SomePid, Reason] ),
+            io:format( "EXIT ~p ~p~n", [ SomePid, Reason ] ),
             loop( X );
         Any ->
-            io:format( "Received:~p~n", [Any] ),
+            io:format( "Received:~p~n", [ Any ] ),
             loop( X )
     end.
