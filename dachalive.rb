d11f33f65524bf2fcf8f7e37b8d4fa0debc88370
yasuhito <dach000@hongo000.logos.ic.i.u-tokyo.ac.jp>
2008-08-01 18:53:28 +0900

    updated.

--- dachalive.rb
+++ dachalive.rb
@@ -14,36 +14,41 @@ if ARGV.size != 1
   $stderr.puts "Usage: dachalive.rb CLUSTER_NAME"
   exit
 end
-cluster_name = ARGV[ 0 ]
+cluster_name = ARGV[ 0 ].to_sym
 
 
-puts 'About to daemonize.'
 fork do
   daemonize
-  log = Tempfile.new( 'dachalive.log' )
-
   loop do
-    Nodes.list( cluster_name ).collect do | each |
-      Thread.start do
-        begin
-          sc = TCPSocket.open( each, 3224 )
-          log.puts sc.gets
-          log.flush
-          sc.close
-        rescue => e
-          log.puts "Exception when connecting to #{ each }: #{ e.inspect }"
-          log.flush
+    socket = TCPServer.open( 3225 )
+    loop do
+      Thread.start( socket.accept ) do | ss |
+        dead = []
+        alive = []
+        Nodes.list( cluster_name ).collect do | each |
+          Thread.start do
+            begin
+              sc = TCPSocket.open( each, 3224 )
+              alive << each
+              sc.close
+            rescue => e
+              dead << each
+            end
+          end
+        end.each do | each |
+          each.join
         end
+
+        ss.puts alive.join( ',' )
+        ss.close
       end
-    end.each do | each |
-      each.join
     end
   end
 end
 
 
-puts 'The subproces has become a daemon.'
-puts "But I'm going to stick around for a while."
-sleep 10
-puts "Okay, now I'm done"
+# puts 'The subproces has become a daemon.'
+# puts "But I'm going to stick around for a while."
+# sleep 10
+# puts "Okay, now I'm done"
 
