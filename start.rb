08443781aa22cc1fb483dde12699008609fa12c1
Yasuhito TAKAMIYA <yasuhito@yasuhito-takamiya-no-macbook.local>
2008-08-07 08:38:05 +0900

    問題を選べるようにした。

--- start.rb
+++ start.rb
@@ -11,14 +11,20 @@ require 'rake'
 require 'shell'
 
 
-def run cluster
+$TRIAL_ID = nil
+
+
+def run cluster, problem
   Popen3::Shell.open do | shell |
-    cmd = "ssh dach000@#{ cluster[ :list ].first }.#{ cluster[ :domain ] } ruby /home/dach000/nova/nova.rb sample0"
+    cmd = "ssh dach000@#{ cluster[ :list ].first }.#{ cluster[ :domain ] } ruby /home/dach000/nova/nova.rb #{ problem }"
 
     shell.on_stdout do | line |
       $stdout.puts line
     end
     shell.on_stderr do | line |
+      if /hongo/=~ cluster[ :list ].first and /Trial ID: (\S*)/=~ line
+        $TRIAL_ID = $1
+      end
       $stderr.puts line
     end
     shell.on_failure do
@@ -32,19 +38,22 @@ def run cluster
 end
 
 
-if ARGV.size == 0
+if ARGV.size == 1
   Clusters.all.map do | each |
     Thread.start do
-      run Clusters.list( each )
+      run Clusters.list( each ), ARGV[ 0 ]
     end
   end.each do | each |
     each.join
   end
 
   sh 'ruby collect.rb'
-  sh 'scp all.result dach@hongo000.logos.ic.i.u-tokyo.ac.jp:/home/dach000/nova/'
-elsif ARGV.size == 1
-  run Clusters.list( ARGV[ 0 ].to_sym )
+  sh 'scp all.result dach000@hongo000.logos.ic.i.u-tokyo.ac.jp:/home/dach000/nova/'
+  
+  $stderr.puts "Checking answer..."
+  sh "ssh dach000@hongo000.logos.ic.i.u-tokyo.ac.jp /home/dach911/dach_api/dach_api --check_ans #{ $TRIAL_ID } /home/dach000/nova/all.result"
+elsif ARGV.size == 2
+  run Clusters.list( ARGV[ 0 ].to_sym) , ARGV[ 1 ]
 else
   $stderr.puts "start.rb [CLUSTER NAME]"
   exit 1
