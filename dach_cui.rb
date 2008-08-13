0e541c8b76eff1cf318a66491fd4f634432dd65d
Yasuhito TAKAMIYA <yasuhito@yasuhito-takamiya-no-macbook.local>
2008-08-14 00:15:52 +0900

    グラフの頭に少し詳しい情報を出すようにした。

--- dach_cui.rb
+++ dach_cui.rb
@@ -6,6 +6,7 @@ require 'log'
 class DachCUI
   SCALE = 5
 
+
   def self.show_status
     system 'tput clear'
     system 'tput cup 0 0'
@@ -18,18 +19,30 @@ class DachCUI
       puts "[#{ each.capitalize } Cluster]"
 
       # CPU status
-      inuse = Log.orange( '#' ) * ( Cluster[ each ].job_inprogress.size / SCALE )
-      node_left = '#' * ( ( Cluster[ each ].node.size - Cluster[ each ].job_inprogress.size ) / SCALE )
-      puts sprintf( "   %3d CPU: %s", Cluster[ each ].node.size, inuse + node_left )
+      cpu_all = Cluster[ each ].node.size
+      cpu_inuse = Cluster[ each ].job_inprogress.size
+      cpu_idle = ( Cluster[ each ].node.size - Cluster[ each ].job_inprogress.size )
+      puts sprintf( "              CPU #{ cpu_status( cpu_inuse, cpu_idle ) }: %s", Log.orange( '#' ) * ( cpu_inuse / SCALE ) + '#' * ( cpu_idle / SCALE ) )
 
       # Job status
-      done = Log.slate( '#' ) * ( Cluster[ each ].job_done.size / SCALE )
-      inprogress = Log.green( '#' ) * ( Cluster[ each ].job_inprogress.size / SCALE )
-      job_left = '#' * ( ( Cluster[ each ].job.size - Cluster[ each ].job_done.size - Cluster[ each ].job_inprogress.size ) / SCALE )
-      puts sprintf( "   %3d Job: %s", Cluster[ each ].job.size, done + inprogress + job_left )
+      job_all = Cluster[ each ].job.size
+      job_done = Cluster[ each ].job_done.size
+      job_inprogress = Cluster[ each ].job_inprogress.size
+      job_left = job_all - job_done - job_inprogress
+      puts sprintf( "Job #{ job_status( job_done, job_inprogress, job_left ) }: %s", Log.slate( '#' ) * ( job_done / SCALE ) + Log.green( '#' ) * ( job_inprogress / SCALE ) + '#' * ( job_left / SCALE ) )
 
       puts
     end
+  end
+
+
+  def self.cpu_status inuse, idle
+    sprintf "(%3d inuse/%3d idle)", inuse, idle
+  end
+
+
+  def self.job_status done, inprogress, left
+    sprintf "(%3d done/%3d inprogress/%3d left)", done, inprogress, left
   end
 end
 
